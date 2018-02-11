1546b2ebfa2cda8231ee2062796663566010bbae
olivialeon <livleon1@gmail.com>
2018-02-10 23:55:24 -0500

    more stuff

--- app/src/main/java/com/bits/MainActivity.java
+++ app/src/main/java/com/bits/MainActivity.java
@@ -50,6 +50,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view){
-        startActivity(new Intent(MainActivity.this, FeedActivity.class));
+        startActivity(new Intent(MainActivity.this, Main2Activity.class));
     }
 }
--- app/src/main/java/com/bits/Profile_Activity.java
+++ app/src/main/java/com/bits/Profile_Activity.java
@@ -1,12 +1,16 @@
 package com.bits;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by solivialeonvitervo on 2/10/18.
@@ -24,6 +28,9 @@ public class Profile_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(com.bits.R.layout.user_profile);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         //initialize the objects
 
         editText = (EditText) findViewById(com.bits.R.id.editText);
@@ -32,6 +39,8 @@ public class Profile_Activity extends AppCompatActivity {
         editText4 = (EditText) findViewById(com.bits.R.id.editText4);
         button = (Button) findViewById(com.bits.R.id.button);
         imageButton = (ImageButton) findViewById(com.bits.R.id.imageButton);
+
+
     }
 
     @Override
@@ -56,3 +65,5 @@ public class Profile_Activity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
