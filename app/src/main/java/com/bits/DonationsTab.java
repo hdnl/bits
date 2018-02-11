ef6eebfc72cca50b899af4b95c05a957aac1496b
olivialeon <livleon1@gmail.com>
2018-02-10 22:14:02 -0500

    tabs_2

---
+++ app/src/main/java/com/bits/DonationsTab.java
@@ -0,0 +1,23 @@
+package com.bits;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by solivialeonvitervo on 2/10/18.
+ */
+
+public class DonationsTab extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.donations_tab, container, false);
+
+    }
+}
